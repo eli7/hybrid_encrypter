9ce364b29c42e0675a8579437c6dbad73aad357d
Daniel <dmail07@gmail.com>
2011-09-22 12:42:02 -0300

    Committing interfaces for AEAD, BlockCipher and MAC, besides the README.

---
+++ BlockCipher.java
@@ -0,0 +1,39 @@
+interface BlockCipher { 
+    /** 
+     * Block size in bits. 
+     */ 
+    int blockBits(); 
+    /** 
+     * Key size in bits. 
+     */ 
+    int keyBits(); 
+    /** 
+     * Setup the cipher key for this block cipher instance. 
+     * 
+     * @param  cipherKey   the cipher key. 
+     * @param  keyBits     size of the cipher key in bits. 
+     */ 
+    void makeKey(byte[] cipherKey, int keyBits);     /** 
+     * Encrypt exactly one block of plaintext. 
+     * 
+     * @param  mBlock      plaintext block. 
+     * @param  cBlock      ciphertext block. 
+     */ 
+    void encrypt(byte[] mBlock, byte[] cBlock); 
+    /** 
+     * Decrypt exactly one block of ciphertext. 
+     * 
+     * @param  cBlock      ciphertext block. 
+     * @param  mBlock      plaintext block. 
+     */ 
+    void decrypt(byte[] cBlock, byte[] mBlock); 
+    /** 
+     * Apply a square-complete transform to exactly
+     * one block of ciphertext. 
+     * 
+     * @param  cBlock      ciphertext block. 
+     * @param  mBlock      plaintext block. 
+     */ 
+    void sct(byte[] cBlock, byte[] mBlock); 
+} 
+
---
+++ MAC.java
@@ -0,0 +1,47 @@
+interface MAC { 
+    /** 
+     * Provide the underlying block cipher. 
+     * 
+     * @param  cipher      the underlying block cipher. 
+     */ 
+    void setCipher(BlockCipher cipher); 
+    /** 
+     * Provide the cipher key. 
+     * 
+     * @param  cipherKey   the cipher key. 
+     * @param  keyBits     the key size in bits. 
+     */ 
+    void setKey(byte[] cipherKey, int keyBits); 
+    /** 
+     * Prepare to authenticate a new message. 
+     */ 
+    void init();     /** 
+     * Prepare to authenticate a new message with a given initial 
+offset value. Behavior is algorithm-dependent, e.g. default Marvin 
+offset value R = E_K(lpad(c)) ^ lpad(c). 
+     * 
+     * @param  R           the initial offset value. 
+     */ 
+    void init(byte[] R); 
+    /** 
+     * Update the MAC tag computation with a message chunk. 
+     * 
+     * @param  aData       data chunk to authenticate. 
+     * @param  aLength     its length in bytes. 
+     */ 
+    void update(byte[] aData, int aLength); 
+    /** 
+     * Complete if necessary the data processing and 
+     * get the MAC tag (of specified size) 
+     * of the whole message provided. 
+     * 
+     * @param  tag         the MAC tag buffer. 
+     * @param  tagBits     the desired tag size in bits. 
+     * 
+     * @return MAC tag of the whole message. 
+     * If the tag parameter is null, a new buffer is automatically 
+allocated, otherwise the input buffer is returned. 
+     */ 
+    byte[] getTag(byte[] tag, int tagBits); 
+} 
+
